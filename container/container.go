9804a0b2613847633cd2944a110af039cef1adcb
Stephane Charite <stephane.charite@ge.com>
2020-02-20 08:59:34 -0500

    creating the structure for container and database interfaces

--- container/container.go
+++ container/container.go
@@ -1,13 +1,77 @@
+// Package container provides functions to create, list, and fetch Cosmos Container resources
+// and retrieve and instance of an container client
 package container
 
-import (
-	"cosmos-go-sdk/item"
-)
-
-type ContainerClient struct {
-	name string
+// Client defines the container client
+type Client struct {
+	name   string
+	dbName string
+	key    string
 }
 
-func (containerClient *ContainerClient) Item(id, partitionKey string) *item.ItemClient {
+// Container defines an cosmos container instance
+type Container struct {
+	id             string         // name of container provided on creation
+	indexingPolicy IndexingPolicy // It is the indexing policy settings for collection.
+	partitionKey   PartitionKey   // It is the partitioning configuration settings for collection.
+	_rid           string         // It is a system generated property. The resource ID (_rid) is a system-generated identifier.
+	_ts            int64          // It is a system generated property. It specifies the last updated timestamp of the resource. The value is a timestamp.
+	_self          string         // It is a system generated property. It is the unique addressable URI for the resource.
+	_etag          string         // It is a system generated property that specifies the resource etag required for optimistic concurrency control.
+	_doc           string         // It is a system generated property that specifies the addressable path of the documents resource.
+	_sprocs        string         // It is a system generated property that specifies the addressable path of the stored procedures (sprocs) resource.
+	_triggers      string         // It is a system generated property that specifies the addressable path of the triggers resource.
+	_udfs          string         // It is a system generated property that specifies the addressable path of the user-defined functions (udfs) resource.
+	_conflicts     string         // It is a system generated property that specifies the addressable path of the conflicts resource. During an operation on a resource within a collection, if a conflict occurs, users can inspect the conflicting resources by performing a GET on the conflicts URI path.
+}
+
+type PartitionKey struct {
+	path {}string
+	king string
+}
+
+type IndexingPolicy struct {
+	automatic     string
+	indexingMode  string
+	includedPaths IncludedPaths
+	excludedPaths ExcludedPaths
+}
+
+type IncludedPaths struct {
+	path 		string
+	dataType 	string // should be enum
+	kind 		string
+	precision 	int
+}
+
+type ExcludedPaths struct {
+	path string
+}
+
+// Create creates an new instance of an cosmos container
+// It returns a Container struct
+func (client *Client) Create(id string) *Container {
 	return nil
 }
+
+// List all available Containers
+// It returns a list of Container structs
+func (client *Client) List() []*Container {
+	return nil
+}
+
+// Get fetches a Container by id
+// It returns a Container struct
+func (client *Client) Get(id string) *Container {
+	return nil
+}
+
+// Delete deletes an container
+// It returns nil
+func (client *Client) Delete() {
+	return nil
+}
+
+// func (containerClient *Client) Item(id, partitionKey string) *item.ItemClient {
+// 	return nil
+// }
--- database/database.go
+++ database/database.go
@@ -1,11 +1,47 @@
+// Package database provides functions to create, list, and fetch Cosmos Database resources
+// and retrieve and instance of an container client
 package database
 
-import "cosmos-go-sdk/container"
-
-type DatabaseClient struct {
+// Client defines the database client
+type Client struct {
 	name string
+	key  string
 }
 
-func (databaseClient *DatabaseClient) Container(name string) *container.ContainerClient {
+// Database defines an cosmos database instance
+type Database struct {
+	id     string // name of database provided on creation
+	_rid   string // It is a system generated property. The resource ID (_rid) is a system-generated identifier.
+	_ts    int64  // It is a system generated property. It specifies the last updated timestamp of the resource. The value is a timestamp.
+	_self  string // It is a system generated property. It is the unique addressable URI for the resource.
+	_etag  string // It is a system generated property that specifies the resource etag required for optimistic concurrency control.
+	_colls string // It is a system generated property that specifies the addressable path of the collections resource.
+	_user  string // It is a system generated property that specifies the addressable path of the users resource.
+}
+
+// Create creates an new instance of an cosmos database
+// It returns a Database struct
+func (client *Client) Create(id string) *Database {
 	return nil
 }
+
+// List all available Databases
+// It returns a list of Database structs
+func (client *Client) List() []*Database {
+	return nil
+}
+
+// Get fetches a Database by id
+// It returns a Database struct
+func (client *Client) Get(id string) *Database {
+	return nil
+}
+
+// Delete an database
+func (client *Client) Delete() {
+}
+
+// Container returns an container client
+// func (client *Client) Container(name string) *container.Client {
+// 	return nil
+// }
