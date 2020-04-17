8eec4ed52a04c9b6e417d9bfaa36c6f1b55a6ede
Nicholas Smith <34406048+nicholasmfsmith@users.noreply.github.com>
2020-04-17 13:29:14 -0400

    Adding PUT request in Rest package (#12)
    * adding PUT progress; missing functional unit tests with mocked external dependencies
    
    * removing manual creation of http mocked request
    
    * adding todo for further validation in future PR
    
    * added validation that mocked reponse body is asserted
    
    * cleaning up resource getters; adjusting unit test; adding util function for extracting resourcePath; adjusting resource interface
    
    * adding todo comment for optional headers
    
    * adding todo comment for additional testing

--- rest/rest.go
+++ rest/rest.go
@@ -2,6 +2,53 @@
 // the HTTP interactions with the Azure API.
 package rest
 
+import (
+	"bytes"
+	"cosmos-go-sdk/rest/internal/token"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
+
+// TODO: [NS] Make Token more mockable
+
+const (
+	// Current version of the Azure Cosmos API used
+	apiVersion = "2017-02-22"
+	// TODO: [NS] Find better ways to handle timeouts
+	// Should this be an input parameters?
+	// Different per HTTP request type?
+	requestTimeout = 30 * time.Second
+)
+
+var (
+	// HTTPClient is the shared HTTP client used for all requests
+	HTTPClient IHttpClient
+)
+
+// IHttpClient is an interface used to override the real HTTP client for testing
+type IHttpClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+// IResource is an interface to define the contract between any resource and this package.
+// All resources must be able to build their URI and provide their resource type, path, and primary key
+type IResource interface {
+	URI() string
+	ResourceType() string
+	PartitionKey() string
+}
+
+// TODO: [NS] Explore better ways to override HTTP client for testing
+func init() {
+	// Initialize the HttpClient to the true http.Client type, unless overridden by a mock for testing
+	HTTPClient = &http.Client{
+		Timeout: requestTimeout,
+	}
+}
+
 // Post performs a POST HTTP request to the Azure API to create the provided resource.
 // It returns the created resource as a byte array and any errors encountered.
 func Post(resource []byte) ([]byte, error) {
@@ -16,10 +63,52 @@ func Get(id string) ([]byte, error) {
 }
 
 // Put performs a PUT HTTP request to the Azure API for the provided
-// resource ID to replace the existing remote resource with the provided resource.
+// resource to replace the existing remote resource with the provided resource.
 // It returns the updated resource as a byte array and any errors encountered.
-func Put(id string, resource []byte) ([]byte, error) {
-	return []byte(""), nil
+// TODO: [NS] How should partitionKey be handled? Should it be optional?
+// TODO: [NS] Add better error messages
+func Put(resource IResource, key string, body []byte) ([]byte, error) {
+	// Get required data points from calling resource, if error return immediately
+	uri := resource.URI()
+	resourceType := resource.ResourceType()
+	resourcePath := extractResourcePathFromURI(uri)
+
+	// Get token, if any error, return immediately
+	// TODO: [NS] Improve Token interface
+	requestToken := token.New(http.MethodPut, resourceType, resourcePath, key)
+	requestTokenBuildErr := requestToken.Build()
+	if requestTokenBuildErr != nil {
+		return nil, requestTokenBuildErr
+	}
+
+	// Notice the format required for the partition Key
+	partitionKey := fmt.Sprintf(`["%s"]`, resource.PartitionKey())
+
+	// Create request
+	req, newRequestErr := http.NewRequest(http.MethodPut, uri, bytes.NewBuffer(body))
+	if newRequestErr != nil {
+		return nil, newRequestErr
+	}
+
+	// Assign required headers
+	req.Header["x-ms-documentdb-partitionkey"] = []string{partitionKey}
+	req.Header["x-ms-version"] = []string{apiVersion}
+	req.Header["x-ms-date"] = []string{strings.ToLower(time.Now().UTC().Format(http.TimeFormat))}
+	req.Header["authorization"] = []string{requestToken.Token}
+	req.Header["content-type"] = []string{"application/json"}
+
+	// TODO: [NS] Handle optional headers
+
+	resp, requestErr := HTTPClient.Do(req)
+	if requestErr != nil {
+		return nil, requestErr
+	}
+	defer resp.Body.Close()
+	respBody, readRespBodyErr := ioutil.ReadAll(resp.Body)
+	if readRespBodyErr != nil {
+		return nil, readRespBodyErr
+	}
+	return respBody, nil
 }
 
 // Delete performs a DELETE HTTP request to the Azure API to remove the resource
@@ -27,4 +116,15 @@ func Put(id string, resource []byte) ([]byte, error) {
 // It returns any errors encountered.
 func Delete(id string) error {
 	return nil
+}
+
+// Note: URI follows the below format:
+// https://{databaseaccount}.documents.azure.com/dbs/{db-id}/colls/{coll-id}/docs/{doc-name}
+// TODO: [NS] Add unit tests
+func extractResourcePathFromURI(uri string) string {
+	res := strings.Split(uri, ".com")
+	if len(res) < 2 {
+		return ""
+	}
+	return res[1]
 }
--- rest/rest_test.go
+++ rest/rest_test.go
@@ -1,24 +1,45 @@
 package rest_test
 
 import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"cosmos-go-sdk/mocks"
 	. "cosmos-go-sdk/rest"
 )
 
 var _ = Describe("Rest", func() {
-	var resource []byte
-	var id string
+	var (
+		mockCtrl       *gomock.Controller
+		mockResource   *mocks.MockIResource
+		mockHttpClient *mocks.MockIHttpClient
+		body           []byte
+		id             string
+		key            string
+	)
 
 	BeforeEach(func() {
-		resource = []byte("This is a test resource")
+		mockCtrl = gomock.NewController(GinkgoT())
+		mockResource = mocks.NewMockIResource(mockCtrl)
+		mockHttpClient = mocks.NewMockIHttpClient(mockCtrl)
+		body = []byte(`{"id": "testID", "partitionKey": "partitionKeyValue", "field1": "value1"}`)
 		id = "1"
+		// NOTE: "dGVzdEtleQ==" -> base64("testKey")
+		key = "dGVzdEtleQ=="
+	})
+
+	AfterEach(func() {
+		mockCtrl.Finish()
 	})
 
 	Context("Post", func() {
 		It("should successfully POST a resource in Azure", func() {
-			testPostResource, testPostError := Post(resource)
+			testPostResource, testPostError := Post(body)
 			Expect(testPostResource).To(Not(BeNil()))
 			Expect(testPostError).To(BeNil())
 		})
@@ -33,9 +54,23 @@ var _ = Describe("Rest", func() {
 	})
 
 	Context("Put", func() {
+		// TODO: [NS] ADD MORE TESTS!
 		It("should successfully PUT a resource in Azure", func() {
-			testPutResource, testPutError := Put(id, resource)
-			Expect(testPutResource).To(Not(BeNil()))
+			// TODO: [NS] Figure out how to mock Token package
+			// Set mocks
+			mockResource.EXPECT().URI().Return("https://mock-test-database-account.documents.azure.com/dbs/{db-id}/colls/{coll-id}/docs/{doc-name}").Times(1)
+			mockResource.EXPECT().ResourceType().Return("testResourceType").Times(1)
+			mockResource.EXPECT().PartitionKey().Return("partitionKeyValue").Times(1)
+
+			// TODO: [NS] Validate proper values are configured in http request passed into Do
+			mockHttpClient.EXPECT().Do(gomock.AssignableToTypeOf(&http.Request{})).Return(&http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(`{"key": "value"}`))),
+			}, nil).Times(1)
+
+			HTTPClient = mockHttpClient
+			testPutResource, testPutError := Put(mockResource, key, body)
+			Expect(testPutResource).To(Equal([]byte(`{"key": "value"}`)))
 			Expect(testPutError).To(BeNil())
 		})
 	})
