d0436a198c50dc703faf67a3c2ce4d339b9e0d1c
Stephane Charite <stephane.charite@ge.com>
2020-04-27 09:04:02 -0400

    fix failing unit test

--- database/database.go
+++ database/database.go
@@ -2,39 +2,59 @@
 // and retrieve and instance of an container client
 package database
 
+import (
+	"cosmos-go-sdk/rest"
+	"encoding/json"
+)
+
+const (
+	ResourceType = "dbs"
+)
+
 // Database defines the database client
 type Database struct {
-	name string
-	key  string
+	URI     string
+	Name    string
+	Key     string
+	Request rest.IRequest
 }
 
 // New returns an instance of Database
-func New(name, key string) *Database {
-	return &Database{
+func New(name, key, uri string) Database {
+	uri = uri + "/" + ResourceType + "/" + name
+	return Database{
+		uri,
 		name,
 		key,
+		rest.New(uri, ResourceType, key),
 	}
 }
 
 // Entity defines an cosmos database entity
 type Entity struct {
-	id     string // name of database provided on creation
-	_rid   string // It is a system generated property. The resource ID (_rid) is a system-generated identifier.
-	_ts    int64  // It is a system generated property. It specifies the last updated timestamp of the resource. The value is a timestamp.
-	_self  string // It is a system generated property. It is the unique addressable URI for the resource.
-	_etag  string // It is a system generated property that specifies the resource etag required for optimistic concurrency control.
-	_colls string // It is a system generated property that specifies the addressable path of the collections resource.
-	_user  string // It is a system generated property that specifies the addressable path of the users resource.
+	ID    string `json:"id"`     // name of database provided on creation
+	RID   string `json:"_rid"`   // It is a system generated property. The resource ID (_rid) is a system-generated identifier.
+	TS    int64  `json:"_ts"`    // It is a system generated property. It specifies the last updated timestamp of the resource. The value is a timestamp.
+	SELF  string `json:"_self"`  // It is a system generated property. It is the unique addressable URI for the resource.
+	ETAG  string `json:"_etag"`  // It is a system generated property that specifies the resource etag required for optimistic concurrency control.
+	COLLS string `json:"_colls"` // It is a system generated property that specifies the addressable path of the collections resource.
+	USER  string `json:"_user"`  // It is a system generated property that specifies the addressable path of the users resource.
 }
 
 // Get fetches a Database by id
 // It returns a Database entity
-func (database *Database) Get(id string) (Entity, error) {
-	return Entity{}, nil
+func (database Database) Read() (Entity, error) {
+	bytes, httpError := database.Request.Get()
+	if httpError != nil {
+		return Entity{}, httpError
+	}
+	var databaseEntity Entity
+	json.Unmarshal(bytes, &databaseEntity)
+	return databaseEntity, nil
 }
 
 // Delete an Database entity
 // It returns deleted database entity
-func (database *Database) Delete() error {
+func (database Database) Delete() error {
 	return nil
 }
--- database/database_test.go
+++ database/database_test.go
@@ -1,31 +1,93 @@
 package database_test
 
 import (
+	"errors"
+	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	. "cosmos-go-sdk/database"
+	"cosmos-go-sdk/testdata/mocks"
 )
 
 var _ = Describe("Database", func() {
+	var (
+		mockCtrl     *gomock.Controller
+		mockRequest  *mocks.MockIRequest
+		testDatabase Database
+	)
 
-	var testDatabase *Database
 	BeforeEach(func() {
-		testDatabase = New("name", "key")
+		mockCtrl = gomock.NewController(GinkgoT())
+		mockRequest = mocks.NewMockIRequest(mockCtrl)
+		testDatabase = Database{
+			URI:     "testID",
+			Name:    "testPartitionKey",
+			Key:     "testKey",
+			Request: mockRequest,
+		}
 	})
 
 	Context("New", func() {
 		It("should successfully return a new instance of a Database Client", func() {
-			Expect(testDatabase).To(BeAssignableToTypeOf(&Database{}))
+			testDatabase = New("testDb", "KEY", "localhost")
+			Expect(testDatabase).To(BeAssignableToTypeOf(Database{}))
+		})
+
+		It("should successfully return a new instance of a Database with a type Request as a property", func() {
+			testDatabase = New("testDb", "KEY", "localhost")
+		})
+
+		It("should successfully return a new instance of a Database with modified uri", func() {
+			baseUri := "localhost"
+			databaseName := "testDb"
+			path := "/dbs/" + databaseName
+			testDatabase = New(databaseName, "KEY", baseUri)
+			Expect(testDatabase.URI).To(Equal(baseUri + path))
+		})
+
+		It("should successfully return a new instance of a Database with passed key", func() {
+			key := "KEY"
+			testDatabase = New("testDb", key, "localhost")
+			Expect(testDatabase.Key).To(Equal(key))
+		})
+
+		It("should successfully return a new instance of a Database with passed name", func() {
+			name := "testDb"
+			testDatabase = New(name, "KEY", "localhost")
+			Expect(testDatabase.Name).To(Equal(name))
 		})
 	})
 
-	Context("Get", func() {
-		It("should successfully fetch an Database entity from an Azure Cosmos DB account", func() {
-			testEntityId := "id"
-			database, testReadError := testDatabase.Get(testEntityId)
-			Expect(testReadError).To(BeNil())
-			Expect(database).ToNot(BeNil())
+	Context("Read", func() {
+		It("should successfully fetch an Database entity", func() {
+			mockRequest.EXPECT().Get().Return([]byte("database"), nil).Times(1)
+			database, testReadError := testDatabase.Read()
+			Expect(testReadError).ShouldNot(HaveOccurred())
+			Expect(database).To(Equal(Entity{
+				ID:    "",
+				RID:   "",
+				TS:    0,
+				SELF:  "",
+				ETAG:  "",
+				COLLS: "",
+				USER:  "",
+			}))
+		})
+
+		It("should return error if unable to successfully fetch an database entity", func() {
+			mockRequest.EXPECT().Get().Return(nil, errors.New("http error")).Times(1)
+			database, testReadError := testDatabase.Read()
+			Expect(database).To(Equal(Entity{
+				ID:    "",
+				RID:   "",
+				TS:    0,
+				SELF:  "",
+				ETAG:  "",
+				COLLS: "",
+				USER:  "",
+			}))
+			Expect(testReadError).Should(HaveOccurred())
 		})
 	})
 
